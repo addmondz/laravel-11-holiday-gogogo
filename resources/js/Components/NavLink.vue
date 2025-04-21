a7d69c8983a67eac6138cb4ff94aaa16ad548fe9
Edmondlang <edmond_wuiming@hotmail.com>
2025-04-21 22:46:21 +0800

    update style

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -9,8 +9,8 @@ const props = defineProps<{
 
 const classes = computed(() =>
     props.active
-        ? 'inline-flex items-center px-3 py-3 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out bg-gray-200'
-        : 'inline-flex items-center px-3 py-3 text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 transition duration-150 ease-in-out hover:bg-gray-100',
+        ? 'inline-flex items-center px-3 py-3 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out bg-indigo-100 text-indigo-800'
+        : 'inline-flex items-center px-3 py-3 text-sm font-medium leading-5 text-gray-800 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 transition duration-150 ease-in-out hover:bg-gray-100',
 );
 </script>
 
