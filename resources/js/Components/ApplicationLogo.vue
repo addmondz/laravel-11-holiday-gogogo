ddc78f9eec73b11061f22450eb110e1dba79cc00
Edmondlang <edmond_wuiming@hotmail.com>
2025-04-25 13:52:39 +0800

    code update

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,3 +1,4 @@
 <template>
-    <img src="/images/Logo-Rectangular-for-website.jpeg" alt="Logo" class="w-full h-full object-contain select-none" draggable="false" >
+    <!-- <img src="/images/Logo-Rectangular-for-website.jpeg" alt="Logo" class="w-full h-full object-contain select-none" draggable="false" > -->
+    <img src="/images/Logo-Rectangular-for-website-transparent.png" alt="Logo" class="w-full h-full object-contain select-none" draggable="false" >
 </template>
