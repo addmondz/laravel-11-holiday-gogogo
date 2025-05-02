ba00643a1310ddf3c0728c4576aec433e8bb1c78
Edmondlang <edmond_wuiming@hotmail.com>
2025-05-02 10:43:41 +0800

    code update

--- public/build/assets/DeleteUserForm-1Wnxum4B.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DeleteUserForm.vue_vue_type_script_setup_true_lang-Dd4j62MB.js";import"./app-BQARf5tf.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./TextInput.vue_vue_type_script_setup_true_lang-BOJjJ5Sy.js";import"./Modal.vue_vue_type_script_setup_true_lang-CHWzPjxJ.js";export{o as default};
--- resources/js/Pages/Packages/Show.vue
+++ resources/js/Pages/Packages/Show.vue
@@ -114,7 +114,7 @@
                                                 {{ route('quotation.with-hash', pkg.uuid) }}
                                             </a>
                                             <button
-                                                @click="copyLink"
+                                                @click="copyLink(route('quotation.with-hash', pkg.uuid))"
                                                 class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                             >
                                                 Copy
@@ -1693,8 +1693,7 @@ watch([selectedSeason, selectedDateType, selectedRoomType], ([newSeason, newDate
     priceForm.room_type = newRoomType;
 });
 
-const copyLink = () => {
-    const link = route('quotation.with-hash', props.pkg.id);
+const copyLink = (link) => {
     navigator.clipboard.writeText(link).then(() => {
         Swal.fire({
             icon: 'success',
