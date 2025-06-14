eb29456e630c5470c36f24a2ce816b15d3ff10aa
Edmond Lang <edmond_wuiming@hotmail.com>
2025-06-15 00:59:52 +0800

    code update

--- resources/views/senangpay/form.blade.php
+++ resources/views/senangpay/form.blade.php
@@ -8,12 +8,12 @@
 <body>
     <form method="POST" action="{{ url('/payment/process') }}">
         @csrf
-        <p><input type="text" name="detail" placeholder="Transaction Detail"></p>
-        <p><input type="text" name="amount" placeholder="Amount (e.g. 10.00)"></p>
-        <p><input type="text" name="order_id" value="{{ time() }}" placeholder="Order ID"></p>
-        <p><input type="text" name="name" placeholder="Customer Name"></p>
-        <p><input type="email" name="email" placeholder="Customer Email"></p>
-        <p><input type="text" name="phone" placeholder="Customer Phone"></p>
+        <p>Transaction Detail: <br><input type="text" name="detail" placeholder="Transaction Detail"></p>
+        <p>Amount: <br><input type="text" name="amount" placeholder="Amount (e.g. 10.00)"></p>
+        <p>Order ID: <br><input type="text" name="order_id" value="{{ time() }}" placeholder="Order ID"></p>
+        <p>Customer Name: <br><input type="text" name="name" placeholder="Customer Name"></p>
+        <p>Customer Email: <br><input type="email" name="email" placeholder="Customer Email"></p>
+        <p>Customer Phone: <br><input type="text" name="phone" placeholder="Customer Phone"></p>
         <button type="submit">Pay Now</button>
     </form>
 </body>
