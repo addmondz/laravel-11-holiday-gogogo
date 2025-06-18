f93eddced9b3971c09519ce93056ef02707b9cb4
Edmond Lang <edmond_wuiming@hotmail.com>
2025-06-18 19:14:27 +0800

    code udpate

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\LogApiActivity::class,
         ]);
 
+        $middleware->validateCsrfTokens(except: [
+            'payment/callback',
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
