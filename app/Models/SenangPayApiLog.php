8cdf12992cdd7fbd153ec539a297d99386c4b8db
Edmond Lang <edmond_wuiming@hotmail.com>
2025-06-21 13:45:05 +0800

    code update

--- app/Models/SenangPayApiLog.php
+++ app/Models/SenangPayApiLog.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class SenangPayApiLog extends Model
 {
-    protected $table = 'senang_pay_api_log';
+    protected $table = 'senang_pay_api_logs';
     protected $fillable = ['log_type', 'status_id', 'order_id', 'transaction_id', 'msg', 'hash', 'raw_payload', 'is_processed'];
     
     protected $casts = [
