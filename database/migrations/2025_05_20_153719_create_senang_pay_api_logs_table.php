d8c74d29ee9c01d629e41a7a69833a67a173d233
Edmond Lang <edmond_wuiming@hotmail.com>
2025-06-21 12:56:43 +0800

    code update

--- database/migrations/2025_05_20_153719_create_senang_pay_api_logs_table.php
+++ database/migrations/2025_05_20_153719_create_senang_pay_api_logs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('senang_pay_api_log', function (Blueprint $table) {
+        Schema::create('senang_pay_api_logs', function (Blueprint $table) {
             $table->id();
             $table->string('log_type')->nullable();
             $table->string('status_id')->nullable();
