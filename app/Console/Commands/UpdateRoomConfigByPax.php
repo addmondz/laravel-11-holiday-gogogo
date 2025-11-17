f733b77d94aaded1e4a44d1fefd42f3b547fd906
Edmond Lang <edmond_wuiming@hotmail.com>
2025-11-17 16:55:30 +0800

    update room config by pax

---
+++ app/Console/Commands/UpdateRoomConfigByPax.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\RoomType;
+use App\Services\CreatePriceConfigurationsService;
+use Illuminate\Console\Command;
+
+class UpdateRoomConfigByPax extends Command
+{
+    protected $signature = 'update:room-config-by-pax';
+    protected $description = 'Update room configurations based on number of pax';
+
+    protected $priceConfigurationService;
+
+    public function __construct(CreatePriceConfigurationsService $priceConfigurationService)
+    {
+        parent::__construct();
+        $this->priceConfigurationService = $priceConfigurationService;
+    }
+
+    public function handle()
+    {
+        $this->info("Starting room config update by pax...");
+
+        RoomType::all()->each(function ($roomType) {
+            $this->info("Updating room config for room type: {$roomType->name}");
+            $this->priceConfigurationService->updateConfigsToPaxAndFill($roomType->id, $roomType->max_occupancy);
+        });
+
+        return Command::SUCCESS;
+    }
+}
